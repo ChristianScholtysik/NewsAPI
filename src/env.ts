ce3df1bb5e088f556c3e39bf9fc5a1d580a27bd9
Christian Scholtysik <christianscholtysik@gmail.com>
2024-06-25 09:17:35 +0200

    delete folder

---
+++ src/env.ts
@@ -0,0 +1,5 @@
+//export const API_KEY = "53802ce8aae74fdfb47ee05bb2673acf";
+// export const API_KEY = "4fe373519647478aa189107be5e94633";
+//export const API_KEY = "7f370bf2855940a8b9d818222fc5ee50";
+//export const API_KEY = "1b329a07c5574d4fa65dfb8e2b11f806";
+export const API_KEY = "aeedae3fa8674e6084b8ad7a9f7c3a97";
